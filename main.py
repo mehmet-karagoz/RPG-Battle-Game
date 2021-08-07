2ed70e03140d44f1a270858a8a0b3b42a5adfc76
mehmet-karagoz <atleownhem@gmail.com>
2021-08-07 19:01:07 +0300

    added priestess and bandit

--- main.py
+++ main.py
@@ -17,10 +17,30 @@ pygame.display.set_caption("RPG Battle")
 background_img = pygame.image.load("img/Background/background.png").convert_alpha()
 
 # functions
-
 #draw background
 def draw_background():
     screen.blit(background_img, (0,0))
+
+#classes
+class Fighter():
+    def __init__(self, x, y, name , max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        img = pygame.image.load(f"img/{self.name}/Idle/0.png")
+        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+priestess = Fighter(220, 130, "Priestess", 80, 30, 3)
+bandit = Fighter(540, 230, "Bandit", 80, 25, 2)
 
 run = True
 
@@ -28,6 +48,10 @@ while run:
 
     clock.tick(fps)
     draw_background()
+
+    # draw fighters
+    priestess.draw()
+    bandit.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
