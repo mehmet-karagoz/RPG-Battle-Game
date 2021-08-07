e7eca1b341aecb93b3457529b83831fb033fc5c4
mehmet-karagoz <atleownhem@gmail.com>
2021-08-07 19:40:10 +0300

    added Idle and Attack animations

--- main.py
+++ main.py
@@ -31,10 +31,36 @@ class Fighter():
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        img = pygame.image.load(f"img/{self.name}/Idle/0.png")
-        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        #Idle
+        temp_list = []
+        for i in range(4):
+            img = pygame.image.load(f"img/{self.name}/Idle/{i}.png")
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #Attack
+        temp_list = []
+        for i in range(7):
+            img = pygame.image.load(f"img/{self.name}/Attack/{i}.png")
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def update(self):
+        animation_cooldown = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
 
     def draw(self):
         screen.blit(self.image, self.rect)
@@ -50,7 +76,9 @@ while run:
     draw_background()
 
     # draw fighters
+    priestess.update()
     priestess.draw()
+    bandit.update()
     bandit.draw()
 
     for event in pygame.event.get():
